043bc071c1e8aa2889f81f68af305fbee71834b7
Gafurov <dlyaregistratsii2020@gmail.com>
2026-09-12 23:48:55 +0500

    Link project to Expo EAS

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,14 @@ const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
 
 export default {
   ...appJson.expo,
+  owner: 'gafurov',
+  slug: 'field-ops-react-native-task',
+  extra: {
+    ...appJson.expo.extra,
+    eas: {
+      projectId: '30f6b867-f407-4990-affb-c015b9527317',
+    },
+  },
   android: {
     ...appJson.expo.android,
     config: googleMapsApiKey ? { googleMaps: { apiKey: googleMapsApiKey } } : undefined,
